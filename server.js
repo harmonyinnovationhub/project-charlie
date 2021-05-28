3924281d9ddc93a5b60d8a102a7d0abe85db6610
Ezenagu <emmanuelezenagu5@gmail.com>
2021-05-28 03:53:47 +0100

    New updates

--- server.js
+++ server.js
@@ -39,7 +39,15 @@ io.on("connection", (socket) => {
 
 });
 
-
+// --> Add this
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 
 
